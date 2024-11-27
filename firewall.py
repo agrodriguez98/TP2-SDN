de1176fd3f48c3112309e898f0b485e92ca92c9f
Agustín <agrodriguez@fi.uba.ar>
2024-11-27 13:44:37 -0300

    Regla 2: Descartar todos los mensajes UDP del host 1 con puerto destino 5001

--- firewall.py
+++ firewall.py
@@ -11,7 +11,7 @@ from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr
+from pox.lib.addresses import EthAddr, IPAddr
 from collections import namedtuple
 import os
 ''' Add your imports here ... '''
@@ -24,16 +24,21 @@ UDP = 17
 TCP = 6
 IPV4 = 0x800
 
-def block_dst_port(port, protocol, event):
+def block_src_address_dst_port(address, port, protocol, event):
     msg = of.ofp_flow_mod()
     msg.match.dl_type = IPV4
+    if address:
+        msg.match.nw_src = IPAddr(address)
     msg.match.nw_proto = protocol
     msg.match.tp_dst = port
     event.connection.send(msg)
 
 def rule_1(event):
-    block_dst_port(80, TCP, event)
-    block_dst_port(80, UDP, event)
+    block_src_address_dst_port(None, 80, TCP, event)
+    block_src_address_dst_port(None, 80, UDP, event)
+
+def rule_2(event):
+    block_src_address_dst_port("10.0.0.1", 5001, UDP, event)
 
 class Firewall (EventMixin):
 
@@ -46,6 +51,7 @@ class Firewall (EventMixin):
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
         rule_1(event)
+        rule_2(event)
 
 def launch ():
     '''
